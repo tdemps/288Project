412ac7c518de608eaabc199fb2e5ae90170a1c7d
tdemps <19tanner@comcast.net>
2018-04-23 09:31:03 -0500

    Update servo.h

--- Project/servo.h
+++ Project/servo.h
@@ -21,23 +21,24 @@
 #include <stdbool.h>
 #include "button.h"
 
+///sets up TIMER1B for servo use
 /**
   * Initializes pin PB5 for use with servo motor.
   * Sets up TIMER1B for PWM output.
-  *
   */
 void servo_init(void);
 
 /**
-  * Returns current angle of servo in degrees.
-  *
+  * Returns current angle (0-180) of the servo motor in degrees.
   */
 int servo_getAngle(void);
 
+///Moves servo and updates angle
 /**
-  * Moves the servo certain number of degrees.
-  * dir > 1 for clockwise, < 1 for counterclockwise
-  *
+  * Moves the servo certain number of degrees and updates angle variable.
+  * dir > 1 for clockwise, < 1 for counterclockwise.
+  *@param degrees  number of degrees to turn the servo motor.
+  *@param dir  direction to turn the motor.
   */
 void servo_moveServo(int degrees, int dir);
 
